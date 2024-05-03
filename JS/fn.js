5fd48b961922af07861be85a173f63f3de9dc7b3
sittnercamila <camilasittmer09@gmail.com>
2024-05-02 21:28:01 -0300

    nuevo ejer

---
+++ JS/fn.js
@@ -0,0 +1,15 @@
+    
+    let valores = [];
+
+function enviar1(numeros){
+    for (i=0; i<= valores.lenght; i++) {
+        console.log(valores[i]) 
+    }
+   // console.log(numeros)
+
+}
+function enviar2(numeros){
+
+    console.log(numeros)
+
+}
